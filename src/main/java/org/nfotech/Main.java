3994bec15b793407b9c924626364209fe03b743b
Fortunate <fortunenwachukwu@gmail.com>
2022-07-24 15:32:53 +0100

    Main class modified

--- src/main/java/org/nfotech/Main.java
+++ src/main/java/org/nfotech/Main.java
@@ -26,16 +26,14 @@ public class Main {
 
 
         Customer Daro = new Customer("Daro", "Decagon Institute", 10000.00);
-//
 //        // To check for out of stock
         Daro.buyProduct(Daro, "Carrot", 5, NfotechStore);
         Daro.buyProduct(Daro, "Chocolate Chip", 5, NfotechStore);
-//
-//
+
+
 //        // A successful Purchase when all conditions are met
         Daro.buyProduct(Daro, "banana", 4, NfotechStore);
-//
-//
+
 //        // Cashier Sell and Print Receipt
         Cashier Ebi = new Cashier("Ebi", 2, Role.CASHIER);
         Ebi.sellProduct(Ebi, Daro);
